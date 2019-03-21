1afa0cc07cec9b3588c66d87d55fd544c26a44ce
alussana <alessandro.lussana@protonmail.com>
2019-03-21 19:14:22 +0100

    wrote ADS/binarySearch.py

---
+++ ADS/binarySearch.py
@@ -0,0 +1,34 @@
+#def binarySearch(A, target):
+#    m = bs(A, target, 0, len(A) - 1)
+#    return(m)
+
+def binarySearch(A, target):
+    i = 0
+    r = len(A)
+    while i < r:
+        m = (i + r) // 2
+        if A[m] == target:
+            return(m)
+        elif target < A[m]:
+            r = m
+        elif target > A[m]:
+            i = m
+    return("NIL")
+
+def bs(A, target, i, r):
+    m = (i + r) // 2
+    print(A, i, r)
+    if A[m] == target:
+        return(m)
+    elif target < A[m]:
+        i = bs(A, target, i, m - 1)
+    elif target > A[m]:
+        i = bs(A, target, m + 1, r)
+    return(m)
+
+if __name__ == '__main__':
+    A = [1,2,3,4,5,6,7,8,9]
+    print(A)
+    target = int(input("Insert the target: "))
+    m = binarySearch(A, target)
+    print("The index of %i in A is %i" %(target, m))
