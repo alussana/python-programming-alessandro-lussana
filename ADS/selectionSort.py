2177b1268b6d31ea40492283a518644fb2e53d19
alussana <alessandro.lussana@protonmail.com>
2019-03-22 15:42:12 +0100

    wrote recursive version of ADS/selectionSort.py

--- ADS/selectionSort.py
+++ ADS/selectionSort.py
@@ -1,6 +1,26 @@
-def selectionSort(A):
+from numpy import inf
+
+def recursiveSelectionSort(A):
+    return(selectionSort(A, 0))
+
+def selectionSort(A, i):
+    if i < len(A):
+        minimum = inf
+        for p in range(i, len(A)):
+            if A[p] <= minimum:
+                minimum = A[p]
+                min_pos = p
+        A[min_pos] = A[i]
+        A[i] = minimum
+        i += 1
+        selectionSort(A, i)
+    return(A)
+
+def iterativeSelectionSort(A):
     pass
 
 if __name__ == '__main__':
     A = [5,2,4,7,1,3,2,6]
-    selectionSort(A)
+    print(A)
+    sorted_A = recursiveSelectionSort(A)    
+    print(sorted_A)
