eff4b469f204e6a3b2086991340c5c21df33b75a
alussana <alessandro.lussana@protonmail.com>
2019-03-22 09:56:10 +0100

    did both iterative and recursive versions of binarySearch alg

--- ADS/binarySearch.py
+++ ADS/binarySearch.py
@@ -1,8 +1,4 @@
-#def binarySearch(A, target):
-#    m = bs(A, target, 0, len(A) - 1)
-#    return(m)
-
-def binarySearch(A, target):
+def iterativeBinarySearch(A, target):
     i = 0
     r = len(A)
     while i < r:
@@ -15,6 +11,10 @@ def binarySearch(A, target):
             i = m
     return("NIL")
 
+def recursiveBinarySearch(A, target):
+    m = bs(A, target, 0, len(A) - 1)
+    return(m)
+
 def bs(A, target, i, r):
     m = (i + r) // 2
     print(A, i, r)
@@ -24,11 +24,11 @@ def bs(A, target, i, r):
         i = bs(A, target, i, m - 1)
     elif target > A[m]:
         i = bs(A, target, m + 1, r)
-    return(m)
+    return(i)
 
 if __name__ == '__main__':
     A = [1,2,3,4,5,6,7,8,9]
     print(A)
     target = int(input("Insert the target: "))
-    m = binarySearch(A, target)
-    print("The index of %i in A is %i" %(target, m))
+    m = recursiveBinarySearch(A, target)
+    print("The index of %i in A is %s" %(target, str(m)))
